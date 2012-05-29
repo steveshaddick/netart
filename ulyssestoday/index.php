75dec7a1a11f8644ed039162829bf2642e29be6f
Steve Shaddick <steve@steveshaddick.com>
2012-05-29 18:05:54 -0400

    ga codes

--- ulyssestoday/index.php
+++ ulyssestoday/index.php
@@ -349,6 +349,11 @@ for ($i=1; $i<=18; $i++)
 	document.body.scrollTop = document.body.scrollHeight;
 	document.documentElement.scrollTop = document.documentElement.scrollHeight;
 </script>
-
+<script>
+    var _gaq=[['_setAccount','UA-2466254-1'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+  </script>
 </body>
 </html>
