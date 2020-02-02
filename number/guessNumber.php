899ef5b31e01d3d8473ce080a82ef4b627928fac
Steve Shaddick <steveshaddick@gmail.com>
2020-02-02 16:09:47 -0500

    some message testing

--- number/guessNumber.php
+++ number/guessNumber.php
@@ -39,23 +39,8 @@ if ($time <= $lastTime) {
 			} else {
 				$guessed = false;
 				$message = "$guess has already been guessed.";
+				$message = "<p>Congratulations!  You have successfully guessed the number! Incredible!</p><p>Would you mind sending an email to steveshaddick@gmail.com to let me know who you are?</p>";
 			}
-			$mail = new PHPMailer();
-			$mail->isSMTP();
-			$mail->Host       = SMTP_HOST;
-			$mail->SMTPAuth   = true;
-			$mail->Username   = SMTP_USERNAME;
-			$mail->Password   = SMTP_PASSWORD;
-			$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-			$mail->Port       = SMTP_PORT;
-
-			$mail->setFrom('number@steveshaddick.com', 'Number.SteveShaddick.com');
-			$mail->addAddress('steveshaddick@gmail.com', 'Steve Shaddick');
-
-			$mail->Subject = 'Testing this numbers email';
-			$mail->Body    = $message;
-			//$mail->AltBody = convert_html_to_text($message);
-			$mail->send();
 		} else {
 			$guessed = true;
 			$mySQL->sendQuery("INSERT INTO Guesses SET guess=$guess, date_entered='$date'");
